a5bdc6b20421bc745e6706c5876cf834af8b1c35
Евгений Гончаров <evg1995gon@gmail.com>
2022-08-30 23:20:08 -0700

    Paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from posts.models import Post, Group
-
+from django.core.paginator import Paginator
 
 def index(request):
     template = 'posts/index.html'
     title = 'Последние обновления на сайте'
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'title': title,
-        'posts': posts}
+        'posts': posts,
+        'page_obj':page_obj,}
 
     return render(request, template, context)
 
