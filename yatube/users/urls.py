4e2353c903d2334a10457414080c0a3552e19e8c
Евгений Гончаров <evg1995gon@gmail.com>
2022-09-02 00:09:31 -0700

    Setting up a password change

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView
 from django.urls import path
-from . import views
+
+from .views import LoginUser, SignUp
 
 app_name = 'users'
 
@@ -8,12 +9,14 @@ urlpatterns = [
     path('logout/',
          LogoutView.as_view(template_name='users/logged_out.html'),
          name='logout'),
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', SignUp.as_view(), name='signup'),
     path('login/',
-         LoginView.as_view(template_name='users/login.html'),
+         LoginUser.as_view(template_name='users/login.html'),
          name='login'
          ),
-    path('password_reset/',
-         PasswordResetView.as_view(template_name='users/password_reset_form.html'),
-         name='password_reset_form'),
+    path('password_change/',
+         PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+         name='password_change_form'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html')),
+    path('password_reset/',PasswordResetView.as_view(template_name='password_reset_form'), name='password_reset_form' )
 ]
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect
 from django.views.generic import CreateView
-
 from django.urls import reverse_lazy
-
 from .forms import CreationForm
+from django.contrib.auth import login
 
 
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('posts:index')
+
+
+class LoginUser(LoginView):
+
+    form_class = AuthenticationForm
+    template_name = 'users/login.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_contex_data(**kwargs)
+    #     return context
+
